33701a99f594153e16c3073a61ca12385c69e587
tianwu <tianwu@huanleguagn.com>
2018-01-02 23:31:50 +0800

    test

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@
  * @Author: lijianzhang
  * @Date: 2018-01-01 21:31:15
  * @Last Modified by: lijianzhang
- * @Last Modified time: 2018-01-02 23:15:12
+ * @Last Modified time: 2018-01-02 23:31:29
  * @flow
  */
 
@@ -40,6 +40,10 @@ class User extends BaseDBModel {
 }
 
 User.init();
+
+User.beforeSave((user: User) => {
+    user.set('password', `${user.get('password')}beforeCreate`);
+});
 
 User.sync({ force: true });
 
